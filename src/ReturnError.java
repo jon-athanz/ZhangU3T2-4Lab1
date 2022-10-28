a2661e5fa2f1538fb5824cb3791e6923ccb6b205
Jonathan Zhang <jonathanz28@nycstudents.net>
2022-10-28 09:30:38 -0400

    U3T2-4Lab1

--- src/ReturnError.java
+++ src/ReturnError.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ReturnError {
+public class ReturnError {
+    private int score;
+
+    public ReturnError(int score) {
+        this.score = score;
+    }
+
+    public String grade() {
+        if (score >= 90) {
+            return "A";
+        } else if (score >= 80) {
+            return "B";
+        } else if (score >= 70) {
+            return "C";
+        } else if (score >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
 }
+
